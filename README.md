a3b92f2767f29db9c3fc1025d4869bfaf853c15e
dev <olof.magnusson94@hotmail.se>
2025-05-21 11:24:22 +0200

    rebase and added working PoC

---
+++ README.md
@@ -0,0 +1,175 @@
+# IDS Rule Inventory Classifier
+- This Python tool analyzes and classifies Suricata rules by detecting key signature types such as hex patterns, string matches, PCREs, and file hashes. It assigns a weighted score to each rule based on its indicators, helping developers evaluate rule strength, specificity, and detection coverage.
+
+## 🛠️ Rule inventory analysis
+- **What it does**: Automatically classifies thousands of IDS rules into category types.
+- **Why it matters**: Helps security teams understand quickly th rulesettructure and focus of their ruleset without manually inspecting each rule.
+- **Usecase**: Ideal for validating signature coverage, identifying rule gaps, or auditing open-source rule collections.
+
+For example, it can analyze rule like:
+```bash
+alert tcp any any -> any any (msg:"ET SHELLCODE Koeln Shellcode"; flow:established; content:"|eb 0f 8b 34 24 33 c9 80 c1|"; content:"|80 36|"; distance:0; content:"|46 e2 fa c3 e8 ec|"; distance:0; classtype:shellcode-detect; sid:2009253; rev:3;)
+alert udp any any -> any any (msg:"ET SHELLCODE Koeln Shellcode (UDP)"; content:"|eb 0f 8b 34 24 33 c9 80 c1|"; content:"|80 36|"; distance:0; content:"|46 e2 fa c3 e8 ec|"; distance:0; classtype:shellcode-detect; sid:2009278; rev:2;)
+```
+
+## ✅ Supported Categories
+
+| Category                 | Description                                        |
+|--------------------------|----------------------------------------------------|
+| PCRE                     | Rules using pcre (regex) detection mechanism       |
+| Hash                     | Content values resembling MD5, SHA1, SHA256 hashes |
+| Hex                      | Rules with hex content payloads (\|00 11 22\|)     |
+| String                   | Readable string matchies                           |
+| Unknown                  | No recognized indicators                           |
+
+## 🧠 How It Works
+Each rule is evaluated through a hierarchical and logical sequence based on well-defined characteristics:
+
+```python
+    PRIORITY = {
+        "PCRE": 100,
+        "Hash": 90,
+        "Hex": 80,
+        "String": 70,
+        "Unknown": 0
+    }
+
+    content_values = re.findall(self.content_values, rule)
+    pcre_values = re.findall(self.pcre_values,rule)
+    matches = []
+
+    for content in content_values:
+        content = content.strip()
+
+        if self.hex_pattern.fullmatch(content):
+            matches.append("Hex")
+        if self.hash_pattern.fullmatch(content):
+            matches.append("Hash")
+        if self.content_pattern.fullmatch(content) and not content.startswith('|'):
+            matches.append("String")
+
+    for _ in pcre_values:
+        matches.append("PCRE")
+
+    if not matches:
+        return {
+                "primary":"Unknown",
+                "total_score":0,
+                "matched":[]
+                }
+
+    match_counts = defaultdict(int)
+    for m in matches:
+        match_counts[m]+=1
+
+    # Score system
+    category_scores = { cat: PRIORITY[cat]*count
+    for cat, count in match_counts.items()}
+
+    primary = max(category_scores, key=category_scores.get)
+    total_score = category_scores[primary]
+```
+
+Example hex pattern match:
+```bash
+alert tcp any any -> any any (msg:"ET SHELLCODE Koeln Shellcode"; flow:established; content:"|eb 0f 8b 34 24 33 c9 80 c1|"; content:"|80 36|"; distance:0; content:"|46 e2 fa c3 e8 ec|"; distance:0; classtype:shellcode-detect; sid:2009253; rev:3;)
+```
+Output where `Hex` was chosen as the primary detection signature:
+```bash
+2025-05-26 11:34:28,261 - INFO - [DEBUG] Category counts: {'Hex': 3}
+2025-05-26 11:34:28,261 - INFO - [DEBUG] Weighted scores: {'Hex': 240}
+2025-05-26 11:34:28,261 - INFO - [DEBUG] Primary:, Hex
+```
+
+By evaluating the checks based on these semantics, it reflects:
+- Suricata’s matching pipeline (e.g., fast pattern matching followed by deeper content or PCRE evaluation).
+- The likely detection outcome of a rule — for example, a rule that relies heavily on hex content suggests the detection is based on specific binary patterns observed in malicious traffic.
+
+## 🔢 Scoring system
+| **Indicator**       | **Score** | **Rationale**                                                                     |
+| ------------------------ | --------- | --------------------------------------------------------------------------------- |
+| **PCRE**                 | 100       | Very specific, rare, and computationally expensive but highly accurate            |
+| **Hash**                 | 90        | Extremely precise; represents known malicious files with low false positive rates |
+| **Hex**                  | 80        | Matches binary patterns; often tied to specific exploits or shellcode             |
+| **String**               | 70        | Matches common strings; useful but can be prone to false positives                |
+| **Unknown**              | 0         | No identifiable signature type detected                                           |
+
+## 📂 File structure
+- classify.py          
+- emerging-all.rules.txt
+- requirements.txt
+- README.md
+- pictures
+
+## ✅ Requirements
+- Python 3.7+
+- Dependencies specified in `requirements.txt`
+
+## 📌 Notes
+- It is under continous development as more features and refinements are about to be added.
+
+## 📦 Usage
+1. Clone the repository on your local machine:
+```
+git clone https://github.com/olofmagn/classify
+```
+
+2. Install requirements:
+```bash
+pip3 -r install requirements.txt
+```
+
+3. Run the script:
+```python
+python3 classify.py -i emerging-all.rules.txt 
+```
+
+Result based on the logic `(alert) && (tcp|udp|icmp|dns|tls)`:
+```
+Protocol: UDP
+Alert type: alert
+Hex: 184
+String: 45
+Unknown: 35
+PCRE: 26
+
+Protocol: TCP
+Alert type: alert
+Hex: 1439
+String: 793
+Unknown: 157
+PCRE: 260
+Hash: 4
+
+Protocol: ICMP
+Alert type: alert
+Hex: 7
+String: 19
+Hash: 1
+PCRE: 2
+Unknown: 1
+
+Protocol: DNS
+Alert type: alert
+Unknown: 16
+Hex: 86
+String: 9393
+PCRE: 66
+
+Protocol: TLS
+Alert type: alert
+String: 5865
+Hash: 106
+Hex: 24
+Unknown: 5
+PCRE: 503
+```
+
+<img src="pictures/udp.png" alt="udp data" width="400"/>
+<img src="pictures/tcp.png" alt="tcp data" width="400"/>
+<img src="pictures/icmp.png" alt="icmp data" width="400"/>
+<img src="pictures/dns.png" alt="dns data" width="400"/>
+<img src="pictures/overall.png" alt="overall data" width="400"/>
+
+## 📝 License
+This project is open-source and licensed under the MIT License. See the LICENSE file for details.
---
+++ classify.py
@@ -0,0 +1,350 @@
+import re
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import argparse
+import sys
+import logging
+
+from typing import List, Optional, TextIO, Tuple, Dict, Any
+
+"""
+Author: Olof Magnusson
+Date: 2025-06-03
+A program that simplifies the understanding of the different rules from open-source data-sets using suricata/snort ids.
+"""
+BANNER = r"""
+   _____ _               _  __
+  / ____| |             (_)/ _|
+ | |    | | __ _ ___ ___ _| |_ _   _
+ | |    | |/ _` / __/ __| |  _| | | |
+ | |____| | (_| \__ \__ \ | | | |_| |
+  \_____|_|\__,_|___/___/_|_|  \__, |
+                                __/ |
+                               |___/
+"""
+class LoggerManager:
+    """
+    Intitalize the logging instance
+
+    Returns: 
+    - The logger associated with this module
+    """
+    def __init__(self, name:str =__name__, level: int = logging.INFO):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+
+        # Avoid duplicate handlers if logger already has one
+        if not self.logger.hasHandlers():
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+
+    def get_logger(self):
+        return self.logger
+
+class SuricataRuleVisualizer:
+    def __init__(self, input_file, output_file: Optional[TextIO]=None) -> int:
+        """
+        Initalize input_file and output_file for rule loading and writing
+        
+        Args:
+        - input_file (str): Path to the input Suricata rule file
+        - output_file (str) : Path to the output file
+
+        Returns:
+        - Initalized logger, files and compiled regex patterns
+        """
+        self.logger = LoggerManager(self.__class__.__name__).get_logger()
+        self.input_file = input_file
+        self.output_file = output_file
+        self.hash_pattern = re.compile(r'[a-fA-F0-9]{32,128}', re.IGNORECASE)
+        self.hex_pattern =  re.compile(r'\|(?:[0-9a-fA-F]{2}\s*)+\|', re.IGNORECASE)
+        self.pcre_values = re.compile(r'pcre\s*:\s*"([^"]+)"', re.IGNORECASE)
+        self.content_pattern = re.compile(r'^(?!\|)(?![a-fA-F0-9]{32,128}$).{4,}$', re.IGNORECASE)
+        self.match_pattern = re.compile(r'^\s*(alert)\s+(tcp|udp|icmp|dns|tls)', re.IGNORECASE) # only IDS alerts
+        self.content_values = re.compile(r'content:\s*"([^"]+)"', re.IGNORECASE)
+
+    def _load_ruleset(self) -> List[str]:
+        """
+        Loads and filters rules from a file.
+
+        Returns: (List[str]) of rule string (e.g., lines starting with 'alert')
+        """
+        try:
+            with open(self.input_file, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+            return lines
+        except FileNotFoundError:
+            self.logger.error(f"File not found: {self.input_file}. Check if you provided correct filepath")
+            sys.exit(1)
+        except IOError:
+            self.logger.error(f"I/O Error occured when reading {self.file_path}. Exiting")
+            sys.exit(1)
+
+    def _parse_rule(self, rule:str) -> Tuple[str, Any]:
+        """
+        Parse protocol and alert type from a rule. We only focus on parsing IDS rules.
+        
+        Args:
+        - rule (str): Rule logic type.
+
+        Returns:
+        - (Tuple[str]): a tuple of strings.
+        """
+        match = re.match(self.match_pattern, rule)
+        if match:
+            alert_type, protocol = match.groups()
+            return protocol, alert_type, rule
+        return None
+
+    def _classify_rule_logic(self, rule:str) -> dict:
+        """
+        Classify rule logic type. 
+
+        Args: 
+        - rule (str): Rule logic type.
+
+        Returns:
+        - primary (dict[str]): The primary alert definition based on the weighted score.
+        """
+        # Priority levels for scoring
+        PRIORITY = {
+            "PCRE": 100,
+            "Hash": 90,
+            "Hex": 80,
+            "String": 70,
+            "Unknown": 0
+        }
+
+        content_values = re.findall(self.content_values, rule)
+        pcre_values = re.findall(self.pcre_values,rule)
+        matches = []
+
+        # Classify content fields
+        for content in content_values:
+            content = content.strip()
+
+            # Hex content pattern (e.g., |00 01 02|)
+            if self.hex_pattern.fullmatch(content):
+                matches.append("Hex")
+            # File hash detection (e.g., 32 to 128 hex chars)
+            if self.hash_pattern.fullmatch(content):
+                matches.append("Hash")
+            # Generic strings (e.g., cmd.exe)
+            if self.content_pattern.fullmatch(content) and not content.startswith('|'):
+                matches.append("String")
+
+        # Classify pcre patterns 
+        for _ in pcre_values:
+            matches.append("PCRE")
+
+        # invalid strings
+        if not matches:
+            return {
+                    "primary":"Unknown",
+                    "total_score":0,
+                    "matched":[]
+                    }
+        
+        match_counts = defaultdict(int)
+        for m in matches:
+            match_counts[m]+=1
+
+        # Score system
+        category_scores = { cat: PRIORITY[cat]*count
+        for cat, count in match_counts.items()}
+
+        primary = max(category_scores, key=category_scores.get)
+        total_score = category_scores[primary]
+
+        return {
+                "primary": primary,
+                "score": total_score,
+                "matched": matches,
+                "counts":dict(match_counts)
+                }
+
+    def group_and_classify_rules(self, rules:list[str]) -> dict:
+        grouped_data = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+
+        for idx, rule in enumerate(rules):
+            rule = rule.strip()
+            parsed = self._parse_rule(rule)
+            if parsed:
+                protocol, alert_type, full_rule = parsed
+                logic = self._classify_rule_logic(full_rule)
+                logic_type = logic["primary"]
+                grouped_data[protocol][alert_type][logic_type] += 1
+
+        return grouped_data
+        
+    def save_or_print_grouped_data(self, grouped_data:Dict[str, Any], output_file:Optional[TextIO]=None) -> int:
+        """
+        Save grouped data to a text file in a structured format.
+        If no file provided, the result will be printed to the console.
+
+        Args:
+        - grouped_data (dict[str]): protocol, alert and rule type.
+        - output_file (file): output file.
+
+        Returns:
+        - Status code (0 = success)
+        """
+        f = open(output_file, 'w') if output_file else None
+        try:
+            for protocol, alert_dict in grouped_data.items():
+                line = f"Protocol: {protocol.upper()}"
+                if f: f.write(f"{line}\n")
+                else: print(line)
+
+                for alert_type, logic_dict in alert_dict.items():
+                    line = f"Alert type: {alert_type}"
+                    if f: f.write(f"{line}\n")
+                    else: print(line)
+
+                    for logic_type, count in logic_dict.items():
+                        line = f"{logic_type}: {count}"
+                        if f: f.write(f"{line}\n")
+                        else: print(line)
+
+                if f: f.write('\n')
+                else: print()
+        finally:
+            if f:
+                f.close()
+                self.logger.info(f"Grouped data saved to {output_file}")
+
+    def visualize_data(self, grouped_data:Dict[str, Any]) -> plt:
+        """
+        Visualize data using stacked bar chart and heatmap.
+
+        Args:
+        - grouped_data (dict[str]): protocol, alert and rule type.
+
+        Returns:
+        - plt: A dataframe with the current plot.
+        """
+        protocols = []
+        alert_types = []
+        logic_types = []
+        counts = []
+
+        for protocol, alert_dict in grouped_data.items():
+            for alert_type, logic_dict in alert_dict.items():
+                for logic_type, count in logic_dict.items():
+                    protocols.append(protocol)
+                    alert_types.append(alert_type)
+                    logic_types.append(logic_type)
+                    counts.append(count)
+
+        # Create DataFrame for visualization
+        data = pd.DataFrame({
+            "Protocol": protocols,
+            "Alert Type": alert_types,
+            "Logic Type": logic_types,
+            "Count": counts
+        })
+
+        # Stacked Bar Chart for each protocol
+        for protocol in data["Protocol"].unique():
+            protocol_data = data[data["Protocol"] == protocol]
+            protocol_pivot = protocol_data.pivot_table(
+                index="Alert Type",
+                columns="Logic Type",
+                values="Count",
+                aggfunc="sum",
+                fill_value=0
+            )
+            protocol_pivot.plot(
+                kind="bar",
+                stacked=True,
+                figsize=(10, 6),
+                title=f"Logic Type Distribution in {protocol.upper()} Protocol"
+            )
+            plt.ylabel("Rule Count")
+            plt.tight_layout()
+            plt.show()
+            # Heatmap
+        heatmap_pivot = data.pivot_table(
+            index=["Protocol", "Alert Type"],
+            columns="Logic Type",
+            values="Count",
+            aggfunc="sum",
+            fill_value=0
+        )
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(heatmap_pivot, annot=True, fmt="d", cmap="YlGnBu")
+        plt.title("Rule Logic Type Distribution by Protocol and Alert Type")
+        plt.ylabel("Protocol & Alert Type")
+        plt.xlabel("Logic Type")
+        plt.tight_layout()
+        plt.show()
+
+class ArgumentParser:
+    """
+    Handles argument parsing
+    """
+    def __init__(self):
+        self.parser = self.create_parser()
+
+    def create_parser(self) -> argparse.ArgumentParser:
+        """
+        Configures the argument parser with expected arguments.
+
+        Returns: An instance of argparse.ArgumentParser
+        """
+        parser = argparse.ArgumentParser(
+                description="Suricata Rule Classifier",
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                epilog=BANNER
+                )
+
+        parser.add_argument('-i','--input_file'
+                            , required=True,
+                            help="Path to the input Suricata rule file."
+                            )
+        parser.add_argument('-o', '--output_file',
+                            help="Path to the output file for saving matched rules"
+                            )
+        return parser
+
+    def parse_args(self) -> argparse.Namespace:
+        """
+        Parse and return command-line arguments
+
+        Returns: a namespace object
+        """
+        return self.parser.parse_args()
+
+class SuricataRuleClassifier:
+    def __init__(self):
+        """ 
+        Initialize the application, including argument parsing and searcher.
+        """
+        parser = ArgumentParser()
+
+        self.args = parser.parse_args()
+        self.visualizer = SuricataRuleVisualizer(input_file = self.args.input_file,
+                                                  output_file = self.args.output_file)
+    def run(self):
+        rules = self.visualizer._load_ruleset()
+        results = self.visualizer.group_and_classify_rules(rules)
+        self.visualizer.save_or_print_grouped_data(results, self.args.output_file)
+        self.visualizer.visualize_data(results)
+
+def main():
+    try:
+        app = SuricataRuleClassifier()
+        app.run()
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by user")
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
